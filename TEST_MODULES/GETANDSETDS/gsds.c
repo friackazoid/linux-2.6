d0a299bb5323a5af0b37686467e6311010165551
jack <jack@DarkStart.(none)>
2010-01-31 17:11:55 +0300

    Add module to test set cs

---
+++ TEST_MODULES/GETANDSETDS/gsds.c
@@ -0,0 +1,48 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+#include <asm/segment.h>
+
+
+static int __init hello_init(void)
+{
+	int cs_s;
+	cs_s = 0;
+
+	printk("[Start test ds]\n");
+		__asm__ __volatile__( "movl %%cs, %0 \n\t"
+					: "=r"(cs_s)
+					);
+	printk("[*] CSis = %X, ( %X ) \n", cs_s, __KERNEL_CS);
+
+	printk("[*] Start set cs: \n");
+		__asm__ __volatile__( "movl %0, %%cs  \n\t"
+					:: "r" (__USER_CS)
+					);
+	printk("[*] Cs is set\n");
+		__asm__ __volatile__( "movl %%cs, %0 \n\t"
+					: "=r"(cs_s)
+					);
+	printk("[*] CSis = %X, ( %X ) \n", cs_s, __USER_CS);
+
+/*
+		__asm__ volatile( "mov %0, %%ax \n\t"
+				:
+				: "r" (cs)
+				: "ax"
+			);
+*/
+
+}
+module_init(hello_init);
+
+static void __exit hello_exit(void)
+{
+
+
+}
+module_exit(hello_exit);
+
+
+MODULE_LICENSE("GPL");
+
