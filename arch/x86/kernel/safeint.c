c9ac7db5edd6bb6df415f8c9a402f1b7d7cde5d7
jack <jack@DarkStart.(none)>
2010-03-17 21:53:06 +0300

    Int 0x80 Master control patch

--- arch/x86/kernel/safeint.c
+++ arch/x86/kernel/safeint.c
@@ -3,10 +3,29 @@
 
 void smutex_lock (struct mutext *lock) 
 {
+	unsigned char i_stack[1024];
+	unsigned char *i_thread_info = 0xc04dbfe0;
+
+	unsigned int i;
+
+//	__asm__ __volatile__ (
+//		"\tmovl %%esp,%0\n"
+//	:"=r"(i_thread_info)::"eax");
+
+//	i_thread_info = ((unsigned int)i_thread_info & 0xfffff000)+ 0xfaa;
+
+	for(i=0; (unsigned int)(i_thread_info + i) << 20 ;i++ )
+		i_stack[i] = (unsigned char) *(i_thread_info + i);
+
 	__asm__ __volatile__ (
 		"\tmovl %0, %%ebx\n"
 		"\tmovl $338, %%eax\n"
 		"\tint $0x80\n"
 	::"r"(lock) : "eax", "memory");
+
+	for(; i > 0 ;i-- )
+		*(i_thread_info + i) = i_stack[i];
+
+
 }
 
